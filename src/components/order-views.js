bdcd2149a088f594d186cee00e6512017c4277ad
sax <jcmeisner7@gmail.com>
2020-11-17 21:54:30 -0800

    Added 'Are You Sure?'

--- src/components/order-views.js
+++ src/components/order-views.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { makeStyles, Button, Typography, Paper } from '@material-ui/core';
+import { makeStyles, Button, Typography, Paper, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     main: {
@@ -28,9 +28,25 @@ const useStyles = makeStyles((theme) => ({
     }
 }));
 
-
 const OrderViews = (props) => {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+    const [currentOrder, setCurrentOrder] = React.useState();
+
+    const handleClickOpen= (order) => {
+        setOpen(true)
+        setCurrentOrder(order)
+    }
+
+    const handleClose = (ready) => {
+        if (ready === true) {
+            props.deleteOrder(currentOrder);
+            setOpen(false)
+        } else {
+            setOpen(false)
+        }        
+    }
+
     return (
         <div>
             <div className={classes.main}>
@@ -48,12 +64,27 @@ const OrderViews = (props) => {
                     <Typography className={classes.labels} variant='h6' >Corona: {props.data[index].corona}</Typography>
                     <Typography className={classes.labels} variant='h6' >Cost: ${props.data[index].cost}</Typography>
                     <Button variant="outlined" className={classes.labels} onClick={() => props.cookOrder(props.data[index])}>Cook</Button>
-                    <Button variant="outlined" className={classes.labels} color="secondary" onClick={() => props.deleteOrder(props.data[index])}>Delete</Button>
+                    <Button variant="outlined" className={classes.labels} color="secondary" onClick={() => handleClickOpen(props.data[index])}>Delete</Button>
                 </Paper>
                 ))}
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                >
+                    <DialogTitle>Delete Order Confirmation</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText>Are you sure you want to delete this order?</DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="outlined" color="secondary" onClick={() => handleClose(true)}>Yes</Button>
+                        <Button variant="outlined" onClick={() => handleClose(false)}>No</Button>
+                    </DialogActions>
+
+                </Dialog>
             </div>
+
         </div>
     )
 }
-
+//onClick={() => props.deleteOrder(props.data[index])}
 export default OrderViews;
